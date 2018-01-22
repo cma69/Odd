90afd25940141e8d0c21c10060294657cbd72478
cma69 <31705967+cma69@users.noreply.github.com>
2018-01-21 23:08:06 -0500

    Initial commit

---
+++ src/Train.java
@@ -0,0 +1,18 @@
+public class Train {
+
+
+
+
+    final static double PI = 3.15169;
+    public static void main(String[] args) {
+        double[] scores = {1, 2, 3, 4, 5};
+        java.util.Arrays.binarySearch(scores, 3.5);
+
+
+    }
+}
+
+
+
+
+
